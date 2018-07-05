1b86ef26b223dd0e5691d4f86bfd960d3baa51bf
Andrie de Vries <apdevries@gmail.com>
2018-07-05 07:30:06 +0000

    version 0.2.1

---
+++ README.md
@@ -0,0 +1,194 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# rrd
+
+[![Build
+Status](https://travis-ci.org/andrie/rrd.svg?branch=master)](https://travis-ci.org/andrie/rrd)
+[![CRAN
+status](https://www.r-pkg.org/badges/version/rrd)](https://cran.r-project.org/package=rrd)
+[![lifecycle](https://img.shields.io/badge/lifecycle-maturing-blue.svg)](https://www.tidyverse.org/lifecycle/#maturing)
+[![](http://www.r-pkg.org/badges/version/rrd)](http://www.r-pkg.org/pkg/rrd)
+[![CRAN RStudio mirror
+downloads](http://cranlogs.r-pkg.org/badges/rrd)](http://www.r-pkg.org/pkg/rrd)
+[![Coverage
+Status](https://img.shields.io/codecov/c/github/andrie/rrd/master.svg)](https://codecov.io/github/andrie/rrd?branch=master)
+
+The `rrd` package allows you to read data from an
+[RRD](http://oss.oetiker.ch/rrdtool/) database.
+
+Internally it uses
+[librrd](http://oss.oetiker.ch/rrdtool/doc/librrd.en.html) to import the
+binary data directly into R without exporting it to an intermediate
+format first.
+
+For an introduction to RRD database, see
+<https://oss.oetiker.ch/rrdtool/tut/rrd-beginners.en.html>
+
+## Installation
+
+### Pre-requisites
+
+In order to build the package from source you need
+[librrd](http://oss.oetiker.ch/rrdtool/doc/librrd.en.html). Installing
+[RRDtool](http://oss.oetiker.ch/rrdtool/) from your package manager will
+usually also install the library.
+
+In ubuntu:
+
+``` sh
+sudo apt-get install rrdtool librrd-dev
+```
+
+In RHEL / CentOS:
+
+``` sh
+sudo yum install rrdtool rrdtool-devel
+```
+
+### Installing
+
+You can install the stable version of the package from CRAN:
+
+``` r
+install.packages("rrd")
+```
+
+And the development version from [GitHub](https://github.com/):
+
+``` r
+# install.packages("devtools")
+devtools::install_github("andrie/rrd")
+```
+
+## Example
+
+In R:
+
+``` r
+library(rrd)
+```
+
+To describe the contents of an RRD file, use `describe_rrd()`:
+
+``` r
+rrd_cpu_0 <- system.file("extdata/cpu-0.rrd", package = "rrd")
+
+describe_rrd(rrd_cpu_0)
+#> A RRD file with 10 RRA arrays and step size 60
+#> [1] AVERAGE_60 (43200 rows)
+#> [2] AVERAGE_300 (25920 rows)
+#> [3] MIN_300 (25920 rows)
+#> [4] MAX_300 (25920 rows)
+#> [5] AVERAGE_3600 (8760 rows)
+#> [6] MIN_3600 (8760 rows)
+#> [7] MAX_3600 (8760 rows)
+#> [8] AVERAGE_86400 (1825 rows)
+#> [9] MIN_86400 (1825 rows)
+#> [10] MAX_86400 (1825 rows)
+```
+
+To read an entire RRD file, i.e. all of the RRA archives, use
+`read_rrd()`. This returns a list of `tibble` objects:
+
+``` r
+cpu <- read_rrd(rrd_cpu_0)
+
+str(cpu, max.level = 1)
+#> List of 10
+#>  $ AVERAGE60   :Classes 'tbl_df', 'tbl' and 'data.frame':    43199 obs. of  9 variables:
+#>  $ AVERAGE300  :Classes 'tbl_df', 'tbl' and 'data.frame':    25919 obs. of  9 variables:
+#>  $ MIN300      :Classes 'tbl_df', 'tbl' and 'data.frame':    25919 obs. of  9 variables:
+#>  $ MAX300      :Classes 'tbl_df', 'tbl' and 'data.frame':    25919 obs. of  9 variables:
+#>  $ AVERAGE3600 :Classes 'tbl_df', 'tbl' and 'data.frame':    8759 obs. of  9 variables:
+#>  $ MIN3600     :Classes 'tbl_df', 'tbl' and 'data.frame':    8759 obs. of  9 variables:
+#>  $ MAX3600     :Classes 'tbl_df', 'tbl' and 'data.frame':    8759 obs. of  9 variables:
+#>  $ AVERAGE86400:Classes 'tbl_df', 'tbl' and 'data.frame':    1824 obs. of  9 variables:
+#>  $ MIN86400    :Classes 'tbl_df', 'tbl' and 'data.frame':    1824 obs. of  9 variables:
+#>  $ MAX86400    :Classes 'tbl_df', 'tbl' and 'data.frame':    1824 obs. of  9 variables:
+```
+
+Since the resulting object is a list of `tibble`s, you can easily work
+with individual data frames:
+
+``` r
+names(cpu)
+#>  [1] "AVERAGE60"    "AVERAGE300"   "MIN300"       "MAX300"      
+#>  [5] "AVERAGE3600"  "MIN3600"      "MAX3600"      "AVERAGE86400"
+#>  [9] "MIN86400"     "MAX86400"
+
+cpu[[1]]
+#> # A tibble: 43,199 x 9
+#>    timestamp              user     sys  nice  idle  wait   irq softirq
+#>  * <dttm>                <dbl>   <dbl> <dbl> <dbl> <dbl> <dbl>   <dbl>
+#>  1 2018-04-02 12:24:00 0.0104  0.00811     0 0.981     0     0       0
+#>  2 2018-04-02 12:25:00 0.0126  0.00630     0 0.979     0     0       0
+#>  3 2018-04-02 12:26:00 0.0159  0.00808     0 0.976     0     0       0
+#>  4 2018-04-02 12:27:00 0.00853 0.00647     0 0.985     0     0       0
+#>  5 2018-04-02 12:28:00 0.0122  0.00999     0 0.978     0     0       0
+#>  6 2018-04-02 12:29:00 0.0106  0.00604     0 0.983     0     0       0
+#>  7 2018-04-02 12:30:00 0.0147  0.00427     0 0.981     0     0       0
+#>  8 2018-04-02 12:31:00 0.0193  0.00767     0 0.971     0     0       0
+#>  9 2018-04-02 12:32:00 0.0300  0.0274      0 0.943     0     0       0
+#> 10 2018-04-02 12:33:00 0.0162  0.00617     0 0.978     0     0       0
+#> # ... with 43,189 more rows, and 1 more variable: stolen <dbl>
+
+tail(cpu$AVERAGE60$sys)
+#> [1] 0.0014390667 0.0020080000 0.0005689333 0.0000000000 0.0014390667
+#> [6] 0.0005689333
+```
+
+To read a single RRA archive from an RRD file, use `read_rra()`. To use
+this function, you must specify several arguments that define the
+specific data to retrieve. This includes the consolidation function
+(e.g. “AVERAGE”) and time step (e.g. 60), the `end` time. You must also
+specifiy either the `start` time, or the number of steps, `n_steps`.
+
+In this example, you extract the average for 1 minute periods (`step
+= 60`), for one entire day (`n_steps = 24 * 60`):
+
+``` r
+end_time <- as.POSIXct("2018-05-02") # timestamp with data in example
+avg_60 <- read_rra(rrd_cpu_0, cf = "AVERAGE", step = 60, n_steps = 24 * 60,
+                     end = end_time)
+
+avg_60
+#> # A tibble: 1,440 x 9
+#>    timestamp              user      sys  nice  idle     wait   irq softirq
+#>  * <dttm>                <dbl>    <dbl> <dbl> <dbl>    <dbl> <dbl>   <dbl>
+#>  1 2018-05-01 00:01:00 0.00458 0.00201      0 0.992 0            0       0
+#>  2 2018-05-01 00:02:00 0.00258 0.000570     0 0.996 0            0       0
+#>  3 2018-05-01 00:03:00 0.00633 0.00144      0 0.992 0            0       0
+#>  4 2018-05-01 00:04:00 0.00515 0.00201      0 0.991 0            0       0
+#>  5 2018-05-01 00:05:00 0.00402 0.000569     0 0.995 0            0       0
+#>  6 2018-05-01 00:06:00 0.00689 0.00144      0 0.992 0            0       0
+#>  7 2018-05-01 00:07:00 0.00371 0.00201      0 0.993 0.00144      0       0
+#>  8 2018-05-01 00:08:00 0.00488 0.00201      0 0.993 0.000569     0       0
+#>  9 2018-05-01 00:09:00 0.00748 0.000568     0 0.992 0            0       0
+#> 10 2018-05-01 00:10:00 0.00516 0            0 0.995 0            0       0
+#> # ... with 1,430 more rows, and 1 more variable: stolen <dbl>
+```
+
+And you can easily plot using your favourite packages:
+
+``` r
+library(ggplot2)
+ggplot(avg_60, aes(x = timestamp, y = user)) + 
+  geom_line() +
+  stat_smooth(method = "loess", span = 0.125, se = FALSE) +
+  ggtitle("CPU0 usage, data read from RRD file")
+```
+
+<img src="man/figures/README-unnamed-chunk-1-1.png" width="100%" />
+
+## More information
+
+For more information on `rrdtool` and the `rrd` format please refer to
+the official [rrdtool
+documentation](http://oss.oetiker.ch/rrdtool/doc/index.en.html) and
+[tutorials](http://oss.oetiker.ch/rrdtool/tut/index.en.html).
+
+You can also read a more in-depth description of the package in an [R
+Views](https://rviews.rstudio.com/) blog post [Reading and analysing log
+files in the RRD database
+format](https://rviews.rstudio.com/2018/06/20/reading-rrd-files/).
---
+++ src/rrd.c
@@ -0,0 +1,423 @@
+#include <rrd.h>
+#include <R.h>
+#include <Rdefines.h>
+#include <Rinternals.h>
+#include <stdio.h>
+
+
+
+// linked list we use to store the rra info
+typedef struct _rra_info {
+  char cf[80];
+  unsigned long rows;
+  unsigned long pdp_per_row;
+  struct _rra_info* next;
+} rra_info; 
+
+
+void free_rra_info(rra_info* rraInfoOut) {
+  while (rraInfoOut) {
+    rra_info* tmp = rraInfoOut;
+    rraInfoOut = rraInfoOut->next;
+    free(tmp);
+  }
+  rraInfoOut = NULL;
+}
+
+
+void print_rra_info(rra_info* rraInfoIn, int count, long unsigned int step) {
+  long unsigned int pdp_per_row;
+  int i;
+  long unsigned int rra_step;
+  
+  Rprintf("A RRD file with %d RRA arrays and step size %ld\n", count, step);
+  
+  rra_info* rra_info_tmp = rraInfoIn;
+  i = 1;
+  while (rra_info_tmp) {
+    pdp_per_row = rra_info_tmp->pdp_per_row;
+    rra_step = step * pdp_per_row;
+    
+    Rprintf("[%d] %s_%ld (%ld rows)\n", i, rra_info_tmp->cf, rra_step, rra_info_tmp->rows);
+    
+    /*
+    Rprintf("- cf: %s\n", rra_info_tmp->cf);
+    Rprintf("- step: %ld\n", rra_step);
+    Rprintf("- rows  : %ld\n", rra_info_tmp->rows);
+    Rprintf("- pdp_per_row: %ld\n", pdp_per_row);
+     */
+    
+    rra_info_tmp = rra_info_tmp->next;
+    i++;
+  }
+}
+
+
+/* reads a rrd_info_t returned by rrdinfo and 
+ * returns the information we need in a rra_info linked list 
+ * returns the rra count and the global step by reference
+ */
+rra_info* get_rra_info(rrd_info_t* rrdInfoIn, int *rraCntOut, unsigned long *stepOut){
+  unsigned long step = 0;
+  int rraCnt = 0;
+  
+  char cfKey[80];
+  char rowsKey[80];
+  char perRowKey[80];
+  
+  //generating key values
+  sprintf(cfKey, "rra[%d].cf", 0);
+  sprintf(rowsKey, "rra[%d].rows", 0);
+  sprintf(perRowKey, "rra[%d].pdp_per_row", 0);
+  
+  rra_info* rraInfoOut;
+  rra_info* rra_info_tmp;
+  
+  rraInfoOut = malloc(sizeof(rra_info)); 
+  if (rraInfoOut == NULL) {
+    Rprintf("error allocating memory\n");
+    return NULL;
+  }
+  
+  rra_info_tmp = rraInfoOut;
+  
+  // note this assumes the entries in rrdInfoIn are in a certain order!
+  // looking at the code of rrdinfo I think we can safely assume this
+  while(rrdInfoIn) {
+    
+    if (!strcmp(rrdInfoIn->key, "step")){
+      step = rrdInfoIn->value.u_cnt;
+    }
+    
+    if (!strcmp(rrdInfoIn->key, cfKey)){
+      if (rraCnt > 0) {
+        rra_info_tmp->next = malloc(sizeof(rra_info));
+        if (rra_info_tmp->next == NULL) {
+          free_rra_info(rraInfoOut);
+          return NULL;
+        }
+        rra_info_tmp = rra_info_tmp->next;
+        rra_info_tmp->next = NULL;
+      }
+      //copying cf string
+      strcpy(rra_info_tmp->cf, rrdInfoIn->value.u_str);
+    }
+    
+    if (!strcmp(rrdInfoIn->key, rowsKey)){
+      // printf("matching rowsKey\n");
+      rra_info_tmp->rows = rrdInfoIn->value.u_cnt;
+    }
+    
+    if (!strcmp(rrdInfoIn->key, perRowKey)){
+      // printf("matching perRowKey\n");
+      rra_info_tmp->pdp_per_row = rrdInfoIn->value.u_cnt;
+      rraCnt++;
+      
+      //generate keys for next RRA
+      sprintf(cfKey, "rra[%d].cf", rraCnt);
+      sprintf(rowsKey, "rra[%d].rows", rraCnt);
+      sprintf(perRowKey, "rra[%d].pdp_per_row", rraCnt);
+    }
+    rrdInfoIn = rrdInfoIn->next;
+  }
+  
+  if (rraCnt == 0) {
+    free(rraInfoOut);
+    rraInfoOut = NULL;
+  }
+  
+  *rraCntOut = rraCnt;
+  *stepOut = step;
+  
+  return rraInfoOut;
+}
+
+
+/* describe the contents of an RRD
+ * 
+ */
+SEXP describe_rrd(SEXP filenameIn){
+  
+  int rraCnt, status, size, i, ds, j, timeStamp;
+  unsigned long curStep, n_datasets, step;
+  rra_info* rra_info_list;
+  rrd_info_t *rrdInfo;
+  
+  char *filename = (char *)CHAR(asChar(filenameIn));
+  
+  //getting rrd info
+  rrdInfo = rrd_info_r(filename);
+  
+  if (rrdInfo == NULL) {
+    error("getting rrd info failed");
+  }
+  
+  //obtain linked list containing the parameters for each RRA
+  rra_info_list = get_rra_info(rrdInfo, &rraCnt, &step);
+  
+  if (rra_info_list == NULL) {
+    free(rrdInfo);
+    error("getting rra info failed\n");
+  }
+  
+  print_rra_info(rra_info_list, rraCnt, step);
+  
+  free_rra_info(rra_info_list);
+  free(rrdInfo);
+  
+  return(R_NilValue);
+}
+
+
+
+/* A wrapper for rrd_fetch_r that first gets some information for the RRD
+ * and uses that information to expose all values in the file
+ * 
+ * Returns a list (one for each RRA) of R data.frames
+ * gets the cosolidation functions and computes the step for each RRA
+ * gets the first and last timestamp for each RRA
+ * calls rrd_fetch_r and copies all values to R data frames
+ * 
+ * https://github.com/oetiker/rrdtool-1.x/blob/master/src/rrd_fetch.c
+ */
+SEXP smart_import_rrd(SEXP filenameIn){
+  
+  int rraCnt, status, size, i, ds, j, timeStamp;
+  unsigned long curStep, n_datasets, step;
+  char **ds_namv; // names of data sources
+  // char *filename;
+  
+  time_t first, last, start, end, *startAr;
+  rrd_value_t *data;
+  rra_info* rra_info_list;
+  rra_info* rra_info_tmp;
+  rrd_info_t *rrdInfo;
+  
+  SEXP out, vec, rraSexpList, rraNames, nam, rowNam, cls;
+  
+  char *filename = (char *)CHAR(asChar(filenameIn));
+
+  //getting last time stamp
+  last = rrd_last_r(filename);
+  
+  //getting rrd info
+  rrdInfo = rrd_info_r(filename);
+  
+  if (rrdInfo == NULL) {
+    error("getting rrd info failed");
+  }
+  
+  //obtain linked list containing the parameters for each RRA
+  rra_info_list = get_rra_info(rrdInfo, &rraCnt, &step);
+  
+  if (rra_info_list == NULL) {
+    free(rrdInfo);
+    error("getting rra info failed\n");
+  }
+  
+  //print_rra_info(rra_info_list, &rraCnt, &step);
+  
+  startAr = malloc(rraCnt * sizeof(time_t));
+  
+  if (startAr == NULL) {
+    free(rrdInfo);
+    free_rra_info(rra_info_list);
+    error("memory allocation error");
+  }
+  
+  //getting first timestamp for each RRA
+  for (i = 0; i < rraCnt; i++) {
+    startAr[i] = rrd_first_r(filename, i);
+  }
+  
+  rra_info_tmp = rra_info_list;
+  PROTECT(rraNames = allocVector(STRSXP, rraCnt));
+  
+  PROTECT(cls = allocVector(STRSXP, 1));
+  SET_STRING_ELT(cls, 0, mkChar("data.frame"));
+  
+  out = PROTECT(allocVector(VECSXP, rraCnt));
+  
+  i = 0;
+  
+  // processing each RRA
+  while (rra_info_tmp) {
+    start = startAr[i];
+    end = last;
+    curStep = step * rra_info_tmp->pdp_per_row;
+    status = rrd_fetch_r(filename, rra_info_tmp->cf, &start, &end, &curStep, &n_datasets, &ds_namv, &data);
+
+    if (status != 0 || data == NULL) {
+      Rprintf("error running rrd_fetch_r\n");
+      free(rrdInfo);
+      free_rra_info(rra_info_list);
+      free(startAr);
+      if (data)
+        free(data);
+      return R_NilValue;
+    }
+    
+    // rrd_fetch does not include start
+    size = (end - start)/curStep - 1;
+    // printf("size %d\n", size);
+    
+    rraSexpList = PROTECT(allocVector(VECSXP, n_datasets + 1));
+    
+    vec = PROTECT(allocVector(INTSXP, size));
+    PROTECT(rowNam = allocVector(STRSXP, size));
+    
+    // rrd_fetch does not include start
+    timeStamp = start + curStep;
+    
+    for (int j = 0; j < size; j++) {
+      INTEGER(vec)[j] = timeStamp;
+      timeStamp += curStep;
+    }
+    
+    // set row names
+    SET_VECTOR_ELT(rraSexpList, 0, vec);
+    setAttrib(rraSexpList, R_RowNamesSymbol, vec);
+    
+    PROTECT(nam = allocVector(STRSXP, n_datasets + 1));
+    SET_STRING_ELT(nam, 0, mkChar("timestamp"));
+    
+    // populating data frames with data
+    for (ds = 0; ds < n_datasets; ds++){
+      SET_STRING_ELT(nam, ds + 1, mkChar(ds_namv[ds]));
+      vec = PROTECT(allocVector(REALSXP, size));
+      
+      for (j = 0; j < size; j++){
+        REAL(vec)[j] = data[ds + j * n_datasets];
+      }
+      // adding ds vector to data frame
+      SET_VECTOR_ELT(rraSexpList, ds + 1, vec);
+    }
+    
+    classgets(rraSexpList, cls);
+    namesgets(rraSexpList, nam);
+    
+    //adding data frame to out
+    SET_VECTOR_ELT(out, i, rraSexpList);
+    
+    //generating data frame labels
+    char rraNameString[80];
+    char stepString[40];
+    
+    sprintf(stepString, "%ld", curStep);
+    strcpy(rraNameString, rra_info_tmp->cf);
+    // strcat(rraNameString, "_");
+    strcat(rraNameString, stepString);
+    SET_STRING_ELT(rraNames, i, mkChar(rraNameString));
+    
+    rra_info_tmp = rra_info_tmp->next;
+    
+    i++;
+    free(data);
+  }
+  
+  setAttrib(out, R_NamesSymbol, rraNames);
+  
+  free_rra_info(rra_info_list);
+  free(startAr);
+  free(rrdInfo);
+  for (int k = 0; k < sizeof(ds_namv)/sizeof(char*); k++) {
+    free(ds_namv[k]);
+  }
+  free(ds_namv);
+  
+  UNPROTECT((n_datasets + 4)*rraCnt + 3);
+  
+  return out;
+}
+
+
+/* A wrapper for rrd_fetch_r
+ * Finds the RRA of interest that best matches cfIn, and stepIn in filenameIn 
+ * then returns an R data.frame that contain the RRA values fitting between 
+ * timestamps startIn (non incl) and sendIn
+ */
+SEXP import_rrd(SEXP filenameIn, SEXP cfIn, SEXP startIn, SEXP endIn, SEXP stepIn)  {
+  
+  rrd_value_t *data;
+  
+  unsigned long step, n_datasets;
+  int status, size, ds, i, timeStamp;
+  
+  // char *filename;
+  // const char *cf;
+  char** ds_namv = NULL;
+  
+  time_t start, end;
+  SEXP out, vec, nam, rowNam, cls;
+  
+  const char *filename  = CHAR(asChar(filenameIn));
+  
+  const char *cf = CHAR(asChar(cfIn));
+  start = (time_t) asInteger(startIn);
+  end = (time_t) asInteger(endIn);
+  
+  step  = (unsigned long) asInteger(stepIn);
+  
+  // calling rrdfetch
+  status = rrd_fetch_r(filename, cf, &start, &end, &step, &n_datasets, &ds_namv, &data);
+  if (status != 0 || data == NULL) {
+    Rprintf("error running rrd_fetch_r\n");
+    // rrd_fetch_r is supposed to free ds_namv
+    // doesn't set to NULL so can't do checks here
+    if (data)
+      free(data);
+    return R_NilValue;
+  }
+  
+  // printf("size of data %d start %d end %d step %d n_datasets %d\n", sizeof(data)/sizeof(rrd_value_t), start, end, step, n_datasets);
+  
+  // turns out rrd_fetch does not include start in data
+  size = (end - start)/step - 1;
+  out = PROTECT(allocVector(VECSXP, n_datasets + 1) );
+  vec = PROTECT(allocVector(INTSXP, size));
+  PROTECT(rowNam = allocVector(STRSXP, size));
+  
+  // turns out rrd_fetch does not include start in data
+  timeStamp = start + step;
+  
+  for (i = 0; i < size; i++) {
+    INTEGER(vec)[i] = timeStamp;
+    timeStamp += step;
+  }
+  
+  // set row names
+  SET_VECTOR_ELT(out, 0, vec);
+  setAttrib(out, R_RowNamesSymbol, vec);
+  
+  // set timestamp columns
+  PROTECT(nam = allocVector(STRSXP, n_datasets + 1));
+  SET_STRING_ELT(nam, 0, mkChar("timestamp"));
+  
+  // set all other columns
+  for (ds = 0; ds < n_datasets; ds++){
+    SET_STRING_ELT(nam, ds + 1, mkChar(ds_namv[ds]));
+    
+    vec = PROTECT(allocVector(REALSXP, size));
+    for (i = 0; i < size; i++){
+      /* // printf("iterating.. i = %d\n", i); */
+      REAL(vec)[i] = data[ds + i*n_datasets];
+    }
+    SET_VECTOR_ELT(out, ds + 1, vec);
+  }
+  
+  // set class and names of data frame 
+  PROTECT(cls = allocVector(STRSXP, 1)); // class attribute
+  SET_STRING_ELT(cls, 0, mkChar("data.frame"));
+  classgets(out, cls);
+  namesgets(out, nam);
+  
+  free(data);
+  for (int k = 0; k < sizeof(ds_namv)/sizeof(char*); k++) {
+    free(ds_namv[k]);
+  }
+  free(ds_namv);
+  UNPROTECT(n_datasets + 5);
+  return out;
+}
+
+
